68e857dbabb53e7d8881a2479c31c65c064b2fe4
Marthe01 <marthe.ouattara@lc.cuny.edu>
2019-10-25 14:03:18 -0400

    Made the part 2 of the Flixster

---
+++ FlixsterApp2/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixsterApp2
+//
+//  Created by Marthe Ouattara on 10/24/19.
+//  Copyright © 2019 Marthe. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
